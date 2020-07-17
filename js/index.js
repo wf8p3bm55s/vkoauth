e01ffdcf5d2cbe71dfd2dcd63555928159ac476d
wf8p3bm55s <wf8p3bm55s@mail.ru>
2020-07-17 22:23:14 +0300

    Edit

--- js/index.js
+++ js/index.js
@@ -1,45 +1,42 @@
 
-class VkAccessToken {
-    constructor(token, tokenExpires) {
-        this.token = token;
-        this.tokenExpires = tokenExpires;
+class JsonpService {
+    static requests = {};
+    static finishRequest(id) {
+        clearTimeout(requests[id].timer);
+        document.head.removeChild(requests[id].script);
+        delete requests[id];
     };
 
-    isValid() {
-        return (new Date()).getTime() < this.tokenExpires;
+    static get(url) {
+        return new Promise((resolve, reject) => {
+            let id;
+            do
+                id = Math.round(Math.random() * Math.pow(10, 16));
+            while(id in JsonpService.requests);
+            const script = document.createElement("script");
+            script.onerror = (error) => {
+                JsonpService.finishRequest(id);
+                reject(error);
+            };
+            requests[id] = {
+                script: script,
+                callback: (result) => {
+                    JsonpService.finishRequest(id);
+                    resolve(result);
+                },
+                timer: setTimeout(() => {
+                    JsonpService.finishRequest(id);
+                    reject(new Error("Timeout"));
+                }, 10000)
+            };            
+            script.src = `${url}&callback=JsonpService.requests[${key}].callback`;
+            document.head.appendChild(script);
+        });
     };
 };
 
-class VkOAuth2Provider {
-    static URL = "https://oauth.vk.com/authorize";
-    static buildAuthorizeUrl(
-        client_id, redirect_uri, display, scope, response_type, state, revoke) { 
-        let url = VkOAuth2Provider.URL;
-        url += `?client_id=${client_id}`;
-        url += `&redirect_uri=${redirect_uri}`;
-        display != undefined ? url += `&display=${display}` : null;
-        scope != undefined ? url += `&scope=${scope}` : null;
-        response_type != undefined ? url += `&response_type=${response_type}` : null;
-        state != undefined ? url += `&state=${state}` : null;
-        revoke != undefined ? url += `&revoke=${revoke}` : null;
-        return url;
-    };
-
-    static authorize(
-        client_id, redirect_uri, display, scope, revoke) {
-        const url = VkOAuth2Provider.buildAuthorizeUrl(
-            client_id, 
-            redirect_uri, 
-            display, 
-            scope, 
-            "token", 
-            new Date().getTime().toString(), 
-            revoke
-        );
-        location.replace(url);
-    };
-
-    static loadTokenFromStorage() {
+class VkAccessToken {
+    static loadFromStorage() {
         const tokenInfoJson = localStorage.getItem("token");
         if(tokenInfoJson === null) {
             const tokenErrorInfoJson = sessionStorage.getItem("tokenError");
@@ -55,39 +52,48 @@ class VkOAuth2Provider {
             parseInt(tokenInfo["state"]) + parseInt(tokenInfo["expires_in"]) * 1000
         );
     };
-};
 
-class JsonpProvider {
-    static callbacks = {};
-    static request(url) {
-        return new Promise((resolve, reject) => {
-            const script = document.createElement("script");
-            let timer;
-            let key;
-            do
-                key = Math.round(Math.random() * Math.pow(10, 16));
-            while(key in JsonpProvider.callbacks);
-            window[key] = (result) => {
-                clearTimeout(timer);
-                document.head.removeChild(script);
-                resolve(result);
-            };
-            script.onerror = (error) => {
-                clearTimeout(timer);
-                document.head.removeChild(script);
-                reject(error);
-            };
-            timer = setTimeout(() => {
-                document.head.removeChild(script);
-                reject(new Error("Timeout"));
-            }, 10000);
-            script.src = `${url}&callback=test`;
-            document.head.appendChild(script);
-        });
+    constructor(token, tokenExpires) {
+        this.token = token;
+        this.tokenExpires = tokenExpires;
+    };
+
+    isValid() {
+        return new Date().getTime() < this.tokenExpires;
     };
 };
 
-class VkApiProvider {
+class VkOAuth2Service {
+    static URL = "https://oauth.vk.com/authorize";
+    static buildAuthorizeUrl(
+        client_id, redirect_uri, display, scope, response_type, state, revoke) { 
+        let url = VkOAuth2Service.URL;
+        url += `?client_id=${client_id}`;
+        url += `&redirect_uri=${redirect_uri}`;
+        display != undefined ? url += `&display=${display}` : null;
+        scope != undefined ? url += `&scope=${scope}` : null;
+        response_type != undefined ? url += `&response_type=${response_type}` : null;
+        state != undefined ? url += `&state=${state}` : null;
+        revoke != undefined ? url += `&revoke=${revoke}` : null;
+        return url;
+    };
+
+    static authorize(
+        client_id, redirect_uri, display, scope, revoke) {
+        const url = VkOAuth2Service.buildAuthorizeUrl(
+            client_id, 
+            redirect_uri, 
+            display, 
+            scope, 
+            "token", 
+            new Date().getTime().toString(), 
+            revoke
+        );
+        location.replace(url);
+    };
+};
+
+class VkApiService {
     static VERSION = "5.120";
     static URL = "https://api.vk.com/method/";
 
@@ -96,24 +102,15 @@ class VkApiProvider {
     };
 
     buildApiUrl(methodName, params) {
-        let url = `${VkApiProvider.URL}${methodName}?`;
+        let url = `${VkApiService.URL}${methodName}?`;
         url += Object.entries(params).map(entry => `${entry[0]}=${entry[1]}`).join("&");
         url += `&access_token=${this.accessToken.token}`;
-        url += `&v=${VkApiProvider.VERSION}`;
+        url += `&v=${VkApiService.VERSION}`;
         return url;
     };
 
     requestApi(methodName, params) {
-        return new Promise((resolve, reject) => {
-            const xhr = new XMLHttpRequest();
-            xhr.responseType = 'json';
-            xhr.timeout = 10000;
-            xhr.open("GET", this.buildApiUrl(methodName, params), true);
-            xhr.send();
-            xhr.onload = () => resolve(xhr);
-            xhr.onerror = () => reject(xhr);
-            xhr.timeout = () => reject(xhr);
-        });
+        return JsonpService.get(this.buildApiUrl(methodName, params));
     };
 };
 
@@ -127,8 +124,7 @@ class ViewEngine {
 
     static getAuthorizedView(model) {
         const button = document.createElement("button");
-        button.innerText = "Авторизоваться";
-        // button.onclick = authorizeBtnCLickCallback;
+        button.innerText = "Авторизован";
         return button;
     };
 
@@ -144,10 +140,10 @@ class ViewEngine {
 };
 
 class AppConfig {
-    constructor(appUrl, redirectPath, appId, rootElementId) {
+    constructor(appId, appUrl, redirectPath, rootElementId) {
+        this.appId = appId;
         this.appUrl = appUrl;
         this.redirectPath = redirectPath;
-        this.appId = appId;
         this.rootElementId = rootElementId;
     };
 };
@@ -159,27 +155,24 @@ class App {
 
     init() {
         this.viewEngine = new ViewEngine(this.config.rootElementId);
-        this.accessToken = VkOAuth2Provider.loadTokenFromStorage();
+        this.accessToken = VkAccessToken.loadFromStorage();
         if(this.accessToken !== null && this.accessToken.isValid()) {
-            this.vkApiProvider = new VkApiProvider(this.accessToken);
+            this.VkApiService = new VkApiService(this.accessToken);
 
-            const executeCode = `var profileInfo = API.account.getProfileInfo();return {"firstName": profileInfo.first_name, "lastName": profileInfo.last_name, "friends": API.friends.get({"order": "random", "count": 5, "fields": "nickname"})};`;
-
-            this.vkApiProvider.requestApi("execute", {
-                code: executeCode
-            }).then((xhr) => {
-                console.log(xhr);
-                // this.viewEngine.setupView(ViewEngine.getAuthorizedView());
-            }, (xhr) => { 
-                console.log(xhr);
+            this.VkApiService.requestApi(
+                "execute.getSelfAnd5RandomFriendNames", {}
+            ).then((result) => {
+                console.log(result);
+            }, (error) => { 
+                console.log(error);
             });
         } else {
             const authorizeBtnCLickCallback = () => 
-                VkOAuth2Provider.authorize(
+                VkOAuth2Service.authorize(
                     this.config.appId, 
                     this.config.appUrl.concat(this.config.redirectPath), 
                     "page", 
-                    undefined,
+                    "friends",
                     1
                 );
 
@@ -194,9 +187,9 @@ class App {
 
 new App(
     new AppConfig(
+        "7540692", 
         "https://wf8p3bm55s.github.io/vkoauth",
         "/oauth.html",
-        "7540692", 
         "root"
     )
 ).init();
