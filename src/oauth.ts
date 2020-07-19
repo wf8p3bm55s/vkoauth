5414a44ff2004f055988973b390f99332113fceb
wf8p3bm55s <wf8p3bm55s@mail.ru>
2020-07-19 20:30:57 +0300

    Edit

--- src/oauth.ts
+++ src/oauth.ts
@@ -3,15 +3,16 @@ import {
     VkOAuth2RedirectParamsError 
 } from "./vk-oauth2-redirect-params";
 
-const params = new window.URLSearchParams(location.hash.slice(1));
-const successParams = VkOAuth2RedirectParamsSuccess.fromURLSearchParams(params);
-if(successParams !== null) {
-    VkOAuth2RedirectParamsSuccess.store(successParams);
-} else {
-    const errorParams = VkOAuth2RedirectParamsError.fromURLSearchParams(params);
-    if(errorParams !== null)
-        VkOAuth2RedirectParamsError.store(errorParams);
+if(new URL(document.referrer).hostname === "oauth.vk.com") {
+    const params = new window.URLSearchParams(location.hash.slice(1));
+    const successParams = VkOAuth2RedirectParamsSuccess.fromURLSearchParams(params);
+    if(successParams !== null) {
+        VkOAuth2RedirectParamsSuccess.store(successParams);
+    } else {
+        const errorParams = VkOAuth2RedirectParamsError.fromURLSearchParams(params);
+        if(errorParams !== null)
+            VkOAuth2RedirectParamsError.store(errorParams);
+    }
 }
-console.log(document.referrer);
-// location.replace(
-//     location.origin.concat(location.pathname.split("/", 2).join("/")));
+location.replace(
+    location.origin.concat(location.pathname.split("/", 2).join("/")));
